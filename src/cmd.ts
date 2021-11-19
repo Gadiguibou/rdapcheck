ee26ba79f028e8def17ee647d3bddf3d81f3e447
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-11-19 18:27:29 -0500

    Run deno fmt

--- src/cmd.ts
+++ src/cmd.ts
@@ -6,11 +6,11 @@ if (Deno.args.length < 1 || Deno.args.includes("--help") || Deno.args.includes("
 }
 
 const domains = Deno.args;
-const domainTldPairs: [string, string][] = domains.map(domain => {
+const domainTldPairs: [string, string][] = domains.map((domain) => {
     const parts = domain.split(".");
     if (parts.length < 2 || parts.length >= 3) {
         console.error(`Could not find the tld for domain '${domain}'`);
-        console.error("All domain names must be fully qualified. For example: \"example.com\"");
+        console.error('All domain names must be fully qualified. For example: "example.com"');
         Deno.exit(2);
     }
     const tld = parts[1];
@@ -19,9 +19,9 @@ const domainTldPairs: [string, string][] = domains.map(domain => {
 
 const bootstrapFile = await mod.getDNSBootstrapFile();
 
-const tlds = domainTldPairs.map(domain_tld_pair => domain_tld_pair[1]) as string[];
+const tlds = domainTldPairs.map((domain_tld_pair) => domain_tld_pair[1]) as string[];
 const uniqueTlds = [...new Set(tlds)];
-const bootstrapServices = uniqueTlds.map(tld => {
+const bootstrapServices = uniqueTlds.map((tld) => {
     const service = mod.getBootstrapServiceForTLD(tld, bootstrapFile);
     if (service === undefined) {
         console.error(`Could not find a bootstrap service for tld '${tld}'`);
@@ -30,14 +30,16 @@ const bootstrapServices = uniqueTlds.map(tld => {
     return service;
 });
 
-const serviceURLs = bootstrapServices.map(bootstrapService => mod.getServiceURLs(bootstrapService)[0]);
+const serviceURLs = bootstrapServices.map((bootstrapService) =>
+    mod.getServiceURLs(bootstrapService)[0]
+);
 
 for (const [index, tld] of uniqueTlds.entries()) {
     const serviceURL = serviceURLs[index];
     const domainsWithThisTld = domainTldPairs
-        .filter(domainTldPair => domainTldPair[1] === tld)
-        .map(domainTldPair => domainTldPair[0]);
-    
+        .filter((domainTldPair) => domainTldPair[1] === tld)
+        .map((domainTldPair) => domainTldPair[0]);
+
     const availabilities = await mod.checkDomainsAvailabilityAsync(serviceURL, domainsWithThisTld);
     for (const [index, availability] of availabilities.entries()) {
         if (availability) {
--- src/mod.ts
+++ src/mod.ts
@@ -1,11 +1,11 @@
 export type BootstrapServiceRegistry = {
-    "version": string, // e.g. "1.0"
-    "publication": string, // e.g. "YYYY-MM-DDTHH:MM:SSZ"
-    "description": string, // e.g. "Some text"
-    "services": BootstrapService[],
-}
+    "version": string; // e.g. "1.0"
+    "publication": string; // e.g. "YYYY-MM-DDTHH:MM:SSZ"
+    "description": string; // e.g. "Some text"
+    "services": BootstrapService[];
+};
 
-export type BootstrapService = [Entry[], ServiceURL[]]
+export type BootstrapService = [Entry[], ServiceURL[]];
 
 export type Entry = string;
 
@@ -13,23 +13,32 @@ export type ServiceURL = string;
 
 export async function getDNSBootstrapFile(): Promise<BootstrapServiceRegistry> {
     const DNS_BOOTSTRAP_FILE_URL = "https://data.iana.org/rdap/dns.json";
-    return await fetch(DNS_BOOTSTRAP_FILE_URL).then(response => response.json());
+    return await fetch(DNS_BOOTSTRAP_FILE_URL).then((response) => response.json());
 }
 
-export function getBootstrapServiceForTLD(tld: string, bootstrapFile: BootstrapServiceRegistry): BootstrapService | undefined {
-    return bootstrapFile.services.find(service => service[0].includes(tld));
+export function getBootstrapServiceForTLD(
+    tld: string,
+    bootstrapFile: BootstrapServiceRegistry,
+): BootstrapService | undefined {
+    return bootstrapFile.services.find((service) => service[0].includes(tld));
 }
 
 export function getServiceURLs(bootstrapService: BootstrapService): ServiceURL[] {
     return bootstrapService[1];
 }
 
-export async function queryServiceForDomain(serviceURL: ServiceURL, domain: string): Promise<Response> {
+export async function queryServiceForDomain(
+    serviceURL: ServiceURL,
+    domain: string,
+): Promise<Response> {
     return await fetch(`${serviceURL}domain/${domain}`);
 }
 
-export async function checkDomainAvailability(serviceURL: ServiceURL, domain: string): Promise<boolean> {
-    return await queryServiceForDomain(serviceURL, domain).then(response => {
+export async function checkDomainAvailability(
+    serviceURL: ServiceURL,
+    domain: string,
+): Promise<boolean> {
+    return await queryServiceForDomain(serviceURL, domain).then((response) => {
         if (response.ok) {
             return false;
         } else if (response.status === 404) {
@@ -40,47 +49,73 @@ export async function checkDomainAvailability(serviceURL: ServiceURL, domain: st
     });
 }
 
-export async function queryServiceForDomainOrRetry(serviceURL: ServiceURL, domain: string, waitMs = 100): Promise<Response> {
+export async function queryServiceForDomainOrRetry(
+    serviceURL: ServiceURL,
+    domain: string,
+    waitMs = 100,
+): Promise<Response> {
     return await resolveOrRetry(() => queryServiceForDomain(serviceURL, domain), waitMs);
 }
 
-export async function checkDomainAvailabilityOrRetry(serviceURL: ServiceURL, domain: string, waitMs = 100): Promise<boolean> {
+export async function checkDomainAvailabilityOrRetry(
+    serviceURL: ServiceURL,
+    domain: string,
+    waitMs = 100,
+): Promise<boolean> {
     return await resolveOrRetry(() => checkDomainAvailability(serviceURL, domain), waitMs);
 }
 
-export async function queryServiceForDomainsAsync(serviceURL: ServiceURL, domains: string[]): Promise<Response[]> {
+export async function queryServiceForDomainsAsync(
+    serviceURL: ServiceURL,
+    domains: string[],
+): Promise<Response[]> {
     return await Promise.all(
-        domains.map(domain => queryServiceForDomainOrRetry(serviceURL, domain))
+        domains.map((domain) => queryServiceForDomainOrRetry(serviceURL, domain)),
     );
 }
 
-export async function checkDomainsAvailabilityAsync(serviceURL: ServiceURL, domains: string[]): Promise<boolean[]> {
+export async function checkDomainsAvailabilityAsync(
+    serviceURL: ServiceURL,
+    domains: string[],
+): Promise<boolean[]> {
     return await Promise.all(
-        domains.map(domain => checkDomainAvailabilityOrRetry(serviceURL, domain))
+        domains.map((domain) => checkDomainAvailabilityOrRetry(serviceURL, domain)),
     );
 }
 
-export async function queryServiceForDomainsSequential(serviceURL: ServiceURL, domains: string[]): Promise<Response[]> {
+export async function queryServiceForDomainsSequential(
+    serviceURL: ServiceURL,
+    domains: string[],
+): Promise<Response[]> {
     const results: Response[] = [];
     return await sequentialize(
         domains
             .map(
-                domain =>
-                    () => queryServiceForDomain(serviceURL, domain)
-                        .then(response => { results.push(response); })
-            )
+                (domain) =>
+                    () =>
+                        queryServiceForDomain(serviceURL, domain)
+                            .then((response) => {
+                                results.push(response);
+                            }),
+            ),
     ).then(() => results);
 }
 
-export async function checkDomainsAvailabilitySequential(serviceURL: ServiceURL, domains: string[]): Promise<boolean[]> {
+export async function checkDomainsAvailabilitySequential(
+    serviceURL: ServiceURL,
+    domains: string[],
+): Promise<boolean[]> {
     const results: boolean[] = [];
     return await sequentialize(
         domains
             .map(
-                domain =>
-                    () => checkDomainAvailability(serviceURL, domain)
-                        .then(response => { results.push(response); })
-            )
+                (domain) =>
+                    () =>
+                        checkDomainAvailability(serviceURL, domain)
+                            .then((response) => {
+                                results.push(response);
+                            }),
+            ),
     ).then(() => results);
 }
 
@@ -93,7 +128,7 @@ async function resolveOrRetry<T>(f: () => Promise<T>, waitMs: number): Promise<T
 }
 
 function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function sequentialize(fs: (() => Promise<void>)[]): Promise<void> {
