1e884c7e1ec30c549dfe9fb4c18bcd92c501d6e5
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-11-19 18:05:14 -0500

    Add function to get the bootstrap service for a TLD

--- mod.ts
+++ mod.ts
@@ -16,3 +16,7 @@ export async function getDNSBootstrapFile(): Promise<BootstrapServiceRegistry> {
     return await fetch(DNS_BOOTSTRAP_FILE_URL).then(response => response.json());
 }
 
+export function getBootstrapServiceForTLD(tld: string, bootstrapFile: BootstrapServiceRegistry): BootstrapService | undefined {
+    return bootstrapFile.services.find(service => service[0].includes(tld));
+}
+
